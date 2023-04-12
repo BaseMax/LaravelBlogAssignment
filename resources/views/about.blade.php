1fd050ab026477d322bb65e8b5be304ced287e93
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-12 23:15:00 +0330

    static about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    @include("layout.css")
+    @include('layout.css')
 </head>
+
 <body>
-    @include("layout.navbar")
-    @include("layout.js")
+
+
+    @include('layout.navbar')
+    <br><br><br>
+    <h1 style="text-align: center">There is About page for this blog...</h1>
+    @include('layout.js')
 </body>
+
 </html>
